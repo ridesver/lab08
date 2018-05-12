50d0b9e23bf0ba44ac9c563326fe80454900479b
Daria Eremitskaya <31545001+ridesver@users.noreply.github.com>
2018-05-12 19:37:46 +0300

    added doxygen.conf

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,5 @@
 #include <fstream>
 #include <iostream>
 
-void print(const std::string& text, std::ostream& out = std::cout); //функция выводит строку в поток 
-void print(const std::string& text, std::ofstream& out); //функция выводит строку в файл
+void print(const std::string& text, std::ostream& out = std::cout); ##функция выводит строку в поток 
+void print(const std::string& text, std::ofstream& out); ##функция выводит строку в файл
